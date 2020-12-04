e7378cab62d19194eaafb67cbaf5df5b8dc30e80
YesidBarragan <yesarmand2006@hotmail.com>
2020-12-04 07:17:07 -0500

    Error import

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 from db.user_db import UserInDB
 from db.user_db import update_user, get_user
+
 from db.transaction_db import TransactionInDB
 from db.transaction_db import save_transaction
+
 from models.user_models import UserIn, UserOut
+
 from models.transaction_models import TransactionIn, TransactionOut
 
 import datetime
+
 from fastapi import FastAPI, HTTPException
 
 api = FastAPI()
@@ -29,6 +33,7 @@ async def get_balance(username: str):
                             detail="El usuario no existe")
     user_out = UserOut(**user_in_db.dict())
     return user_out
+    
 
 @api.put("/user/transaction/")
 async def make_transaction(transaction_in: TransactionIn):
