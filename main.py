8b18ab7a44a3afeb53cb31dce80dc9c97eddd81a
YesidBarragan <yesarmand2006@hotmail.com>
2020-12-09 08:19:45 -0500

    Políticas CORS

--- main.py
+++ main.py
@@ -8,11 +8,22 @@ from models.user_models import UserIn, UserOut
 
 from models.transaction_models import TransactionIn, TransactionOut
 
+from fastapi.middleware.cors import CORSMiddleware
+
 import datetime
 
 from fastapi import FastAPI, HTTPException
 
 api = FastAPI()
+
+origins = [
+    "http://localhost.tiangolo.com", "https://localhost.tiangolo.com",
+    "http://localhost", "http://localhost:8080",
+]
+api.add_middleware(
+    CORSMiddleware, allow_origins=origins,
+    allow_credentials=True, allow_methods=["*"], allow_headers=["*"],
+)
 
 @api.post("/user/auth/")
 async def auth_user(user_in: UserIn):
